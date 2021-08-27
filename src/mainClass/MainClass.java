6f1db4389cd6a1c33bd03c324a89283acdd6c9f0
raina <skgus5598@gmail.com>
2021-08-27 15:02:42 +0900

    프로젝트 생성

---
+++ src/mainClass/MainClass.java
@@ -0,0 +1,24 @@
+package mainClass;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("1.강동원 2.김도윤 3.김나현\n");
+		System.out.print("숫자 입력 : ");
+		int i = sc.nextInt();
+		
+		switch(i) {
+		
+		case 1 : break;
+		case 2 : break;
+		case 3 : break;
+		
+		}
+	}
+
+}
